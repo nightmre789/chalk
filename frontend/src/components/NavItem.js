a4e537a290ed36ae87eeac1a6525f67a3165896a
nightmre789 <nightmre789@gmail.com>
2020-10-27 21:59:46 +0500

    nav ui

--- frontend/src/components/NavItem.js
+++ frontend/src/components/NavItem.js
@@ -5,7 +5,7 @@ export default props => {
    return (
       <li
          className={
-            "group flex items-center justify-center w-auto px-4 py-4 rounded-xl md:gap-x-2 lg:gap-x-4 font-semibold" +
+            "group flex flex-col lg:flex-row items-center justify-center w-auto px-4 text-center py-4 rounded-xl font-semibold cursor-pointer lg:text-left lg:gap-x-4" +
             (props.active ? " text-white bg-pink-600" : " text-gray-500")
          }
       >
@@ -13,11 +13,18 @@ export default props => {
             src={require("../assets/icons/" +
                (props.icon ? props.icon : "dashboard") +
                ".svg")}
-            className="fill-current group-hover:text-pink-500"
+            className={
+               "fill-current" +
+               (props.active ? "" : " group-hover:text-pink-600")
+            }
          />
-         <a href="##" className="flex-1 group-hover:text-pink-500">
+         <p
+            className={
+               "flex-1" + (props.active ? "" : " group-hover:text-pink-600")
+            }
+         >
             {props.label}
-         </a>
+         </p>
       </li>
    );
 };
