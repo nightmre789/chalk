fa2af8884b860bcee34eb4417ca910abac3a1b3b
HasanBurney <48687738+HasanBurney@users.noreply.github.com>
2020-12-07 19:59:50 +0500

    Update admin.py

--- backend/chalk/api/admin.py
+++ backend/chalk/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .student import Student
+from .courses import Courses
 
 # Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    pass
