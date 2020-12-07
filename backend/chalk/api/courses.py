df06c7f3bfee37049027490740f0197d6e34e73d
HasanBurney <48687738+HasanBurney@users.noreply.github.com>
2020-12-07 19:59:53 +0500

    Update courses.py

--- backend/chalk/api/courses.py
+++ backend/chalk/api/courses.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django_extensions.db.models import TimeStampedModel ,AutoSlugField
 
-class courses(TimeStampedModel):
+class Courses(TimeStampedModel):
     name = models.CharField(max_length=20)
-    code = models.CharField(max_length=10,unique=True)
+    code = models.CharField(max_length=10,unique=True, primary_key=True)
+    credit_hours = models.IntegerField()
+    decription = models.CharField(max_length=100)
+    semester = models.IntegerField()
+
     
 
 
