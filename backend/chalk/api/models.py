1c2ca04182463bb92688990a336242a10fd8302c
HasanBurney <48687738+HasanBurney@users.noreply.github.com>
2020-12-11 19:52:44 +0500

    Update models.py

--- backend/chalk/api/models.py
+++ backend/chalk/api/models.py
@@ -16,17 +16,13 @@ class Student(TimeStampedModel):
 class Department(TimeStampedModel):
     dep_id = models.AutoField(unique=True)
     campus_id = models.ForeignKey(Campus,on_delete=models.CASCADE)
-    students = models.ManyToManyField(Student_Reg, through='Degree_Program') 
+    students = models.ManyToManyField(Student, through='Degree_Program') 
 
 class Degree_Program(TimeStampedModel):
     deg_id = models.CharField(max_length=10,primary_key=True)
     degree_name = models.CharField(max_length=100)
     dep_id = models.ForeignKey(Department,on_delete=models.CASCADE)
-
-class Student_Reg(TimeStampedModel):
     student_id = models.ForeignKey(Student,on_delete=models.CASCADE)
-    degree_id = models.ForeignKey(Degree_Program,on_delete=models.CASCADE)
-    department_id = models.ForeignKey(Department,on_delete=models.CASCADE)
 
 class Semester(TimeStampedModel):
     semester_id = models.CharField(max_length=10,primary_key=True)
@@ -43,16 +39,17 @@ class Courses(TimeStampedModel):
     code = models.CharField(max_length=10, primary_key=True)
     credit_hours = models.IntegerField()
     decription = models.CharField(max_length=100)
-    semester = models.IntegerField()
+    semester_id = models.ForeignKey(Semester,on_delete=models.CASCADE)
 
 class Class(TimeStampedModel):
     class_id = models.TextField(primary_key=True)
     teacher_id = models.ForeignKey(Teacher,on_delete=models.CASCADE)
     course_code = models.ForeignKey(Courses, on_delete=models.CASCADE )
+    students = models.ManyToManyField(Student, through='ClassTaken')
 
 class ClassTaken(TimeStampedModel):
     student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
     class_id = models.ForeignKey(Class,on_delete=models.CASCADE)
     marks = models.IntegerField()
     feedback = models.TextField(max_length=250)
-    students = models.ManyToManyField(Student_Reg, through='Class Taken')
+
