9c3351a4a01ed70184410d30036aefd0f1af130f
HasanBurney <48687738+HasanBurney@users.noreply.github.com>
2020-12-11 23:19:14 +0500

    Update models.py

--- backend/chalk/api/models.py
+++ backend/chalk/api/models.py
@@ -12,7 +12,10 @@ class Student(TimeStampedModel):
     student_id = models.CharField(max_length = 10,unique=True,null=True,blank=True)
     name = models.CharField(max_length=20)
     date_of_birth = models.DateField()
-    cnic = models.IntegerField(unique=True,) 
+    cnic = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return self.student_id ,self.name
 
 class Department(TimeStampedModel):
     dep_id = models.AutoField(primary_key=True)
