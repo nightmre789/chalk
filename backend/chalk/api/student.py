e19df366e556641a340483d5925d4824c2696c90
HasanBurney <48687738+HasanBurney@users.noreply.github.com>
2020-12-11 16:38:52 +0500

    Update student.py

--- backend/chalk/api/student.py
+++ backend/chalk/api/student.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.conf import settings
+from course import Course
 from django_extensions.db.models import TimeStampedModel ,AutoSlugField
 
 class Student(TimeStampedModel):
-    id = models.TextField(max_length = 10,unique = True,primary_key=True)
+    student_id = models.TextField(max_length = 10,primary_key=True)
     name = models.CharField(max_length=20)
     date_of_birth = models.DateField()
     cnic = models.IntegerField(unique=True,)
+
+class Class(TimeStampedModel):
+    class_id = models.TextField(primary_key=True)
+    teacher_id = models.ForeignKey(Teacher,on_delete=CASCADE)
+    course_id = models.ForeignKey(Course)
+
 
     
