7744a1e37e5f58a70d827b00b1ed794adc4ce818
HasanBurney <48687738+HasanBurney@users.noreply.github.com>
2020-12-12 15:34:58 +0500

    Update settings.py

--- backend/chalk/settings.py
+++ backend/chalk/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    "django.contrib.staticfiles", # Required for GraphiQL
+    "graphene_django"
     'chalk.api',
 ]
 
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    "SCHEMA": "django_root.schema.schema"
+}
